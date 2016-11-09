13a21ae75b3fb9062c4319ff12796dac9f8e5d0a
lucifero55 <claudiotazza@gmail.com>
2016-11-09 14:18:28 +0100

    M

---
+++ reverse_input/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+  int i, vettore[5] = {1, 2, 3, 4, 5};
+
+  for (i = 4; i >= 0; i--){
+    printf("%d", vettore[i]);
+  }
+
+  printf("\n\n");
+
+
+
+
+
+
+
+  return 0;
+}
