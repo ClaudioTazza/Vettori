9d24c4616d2e4505a979d8eae231d8c393a12327
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-11-09 14:05:00 +0100

    aggiustamenti

--- NumeriPrimi/main.c
+++ NumeriPrimi/main.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
-int main(){
-
+int main()
+{
   int i, conta, vettore[1000];
 
-  for(i = 0; i < 1000; i++){
-    if (i%2 == 0){
-      vettore[i] = 0;}
+  /* inizializzazione */
+  for(i = 0; i < 1000; i++) vettore[i] = 1;
 
-    else{
-      vettore[i] = 1;}
-  }
-
- for(i = 2; i < 500; i++){
+  /* algoritmo: crivello di Erastotene */
+  for(i = 2; i < 500; i++){
     if (vettore[i] == 1){
-
       for(conta = i+i; conta < 1000; conta += i){
-        vettore[conta] = 0;}
-
+        vettore[conta] = 0;
+      }
     }
   }
 
-  for(i = 0; i < 1000; i++){
-    printf("%6d\t%6d\n", i, vettore[i]);}
+  for(i = 0; i < 1000; i++) {
+    if (vettore[i]==1) {
+      printf("%6d\t%6d\n", i, vettore[i]);
+    }
+  }
 
   return 0;
 }
