57bbffc1f7de13acadcbae3de73e7fcae01597cb
lucifero55 <claudiotazza@gmail.com>
2016-11-13 13:48:38 +0100

    M

--- myvect/main.c
+++ myvect/main.c
@@ -7,9 +7,9 @@ int main(){
   i = 50; 
   myvect_init();
 
-  myvect_vec_in_range(vettore[50], 5, 10, 50);
+  myvect_vec_in_range(&vettore[50], 5, 10, 50);
 
-  myvect_print(vettore[50], 50);
+  myvect_print(&vettore[50], 50);
 
 
   return 0;
